12c1d2382b21453268b705471789b410f33ab434
jboe26 <joshrboepple@outlook.com>
2019-10-14 16:08:05 -0400

    fixed folder layout

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var path = require("path");
 // Sets up the Express App
 
 var app = express();
-// var app = path();
+var app = path();
 var PORT = 3000;
 
 // Sets up the Express app to handle data parsing
